0e701f37a6ed0e5512ae6ef9416b0bede957a973
Beau Shirdavani <beau.shirdavani@bellevuecollge.edu>
2017-11-23 14:07:44 -0800

    LinkedStack header file
    no progress yet, just started

---
+++ LinkedStack.h
@@ -0,0 +1,23 @@
+//****************************************************
+//
+//	Author:		Beau Shirdavani
+//	Date:		11-23-2017
+//	Title:		Assignment 3_1
+//				LinkedStack.h
+//
+//****************************************************
+
+template <class T>
+struct node{
+	T data;
+	node<T>* next;
+}
+
+template <class T>
+class LinkedStack{
+	Node<T>* top;
+public:
+	// provide function prototypes for a Stack here
+}
+
+// provide function implementations here
