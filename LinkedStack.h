8a37625faf4c875a8bb02da8ae845754bdbfbede
Beau Shirdavani <beau.shirdavani@bellevuecollge.edu>
2017-11-24 00:06:14 -0800

    update #2
    file appears to be done. Might have to add 1 or 2 more functions though...

--- LinkedStack.h
+++ LinkedStack.h
@@ -7,6 +7,14 @@
 //
 //****************************************************
 
+
+#ifndef LINKEDSTACK_H_
+#define LINKEDSTACK_H_
+#include <iostream>
+using namespace std;
+
+
+
 template <class T>
 struct node{
 	T data;
@@ -18,6 +26,86 @@ class LinkedStack{
 	Node<T>* top;
 public:
 	// provide function prototypes for a Stack here
+	LinkedStack();
+	bool isEmpty();
+	void push(T&);
+	T pop();
+	template <class U>
+	friend ostream& operator<<(ostream& os, LinkedStack<U>& lStack);
+	T seeTop();
+	~LinkedStack();
 }
 
 // provide function implementations here
+
+//****************************************************
+//										constructor
+template <class T>
+LinkedStack<T>::LinkedStack(){
+	top = NULL;
+}
+
+//****************************************************
+//										isEmpty()
+template <class T>
+LinkedStack<T>::isEmpty(){
+	return ( top == NULL );
+}
+
+//****************************************************
+//										push(T&)
+template <class T>
+void LinkedStack<T>::push(T& item){
+	Node<T>* newNode = new node<T>();
+	newNode->data = item;
+	newNode->next = top;
+	top = newNode;
+}
+
+//****************************************************
+//										pop()
+template <class T>
+T LinkedStack<T>::pop(){
+	if ( !( this.isEmpty() ) ){
+		T topData = top->data;
+		top = top->next;
+		return topData;
+	}
+	else {
+		cout << "Stack is empty, nothing to pop..." << endl;
+	}
+}
+
+//****************************************************
+//										<< operator
+template <class T>
+ostream& operator<<(ostream& os, LinkedStack<T>& lStack){
+	for( int i=0; i < lStack.top; i++ ){
+		os << lStack.array[i] << " ";
+	}
+	return os;
+}
+
+
+//****************************************************
+//										seeTop()
+template <class T>
+T LinkedStack<T>::seeTop(){
+	if ( !( this.isEmpty() ) ){
+		return top->data;
+	} 
+}
+
+
+
+
+
+#endif /*LINKEDSTACK_H_*/
+
+
+
+
+
+
+
+
