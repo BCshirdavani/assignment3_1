0713e7cfd48f76f7b6ac69bed93b88668c261547
Beau Shirdavani <beau.shirdavani@bellevuecollge.edu>
2017-11-26 17:32:27 -0800

    updated since finding bugs on part 2
    Part 2 of assignment 3 revealed some typos as I was compiling. Fixed now.

--- LinkedStack.h
+++ LinkedStack.h
@@ -1,9 +1,9 @@
 //****************************************************
 //
-//	Author:		Beau Shirdavani
-//	Date:		11-23-2017
-//	Title:		Assignment 3_1
-//				LinkedStack.h
+//    Author:        Beau Shirdavani
+//    Date:        11-23-2017
+//    Title:        Assignment 3_1
+//                LinkedStack.h
 //
 //****************************************************
 
@@ -11,99 +11,140 @@
 #ifndef LINKEDSTACK_H_
 #define LINKEDSTACK_H_
 #include <iostream>
+#include <cassert>
 using namespace std;
 
 
 
 template <class T>
 struct node{
-	T data;
-	node<T>* next;
-}
+    T data;
+    node<T>* next;
+};
 
 template <class T>
 class LinkedStack{
-	Node<T>* top;
+    node<T>* top;
 public:
-	// provide function prototypes for a Stack here
-	LinkedStack();
-	bool isEmpty();
-	void push(T&);
-	T pop();
-	// T sTop();
-	template <class U>
-	friend ostream& operator<<(ostream& os, LinkedStack<U>& lStack);
-	T seeTop();
-	~LinkedStack();
-}
+    // provide function prototypes for a Stack here
+    LinkedStack();
+    bool isEmpty();
+    void push(T&);
+    int length;
+    int getLength();
+    T pop();
+    // T sTop();
+    template <class U>
+    friend ostream& operator<<(ostream& os, LinkedStack<U>& lStack);
+    T seeTop();
+    ~LinkedStack();
+};
 
 // provide function implementations here
 
 //****************************************************
-//										constructor
+//                                        constructor
 template <class T>
 LinkedStack<T>::LinkedStack(){
-	top = NULL;
+    top = NULL;
+    length = 0;
 }
 
 //****************************************************
-//										isEmpty()
+//                                        desctructor
 template <class T>
-LinkedStack<T>::isEmpty(){
-	return ( top == NULL );
+LinkedStack<T>::~LinkedStack(){
+    while ( top != NULL ){
+        node<T>* temp;
+        temp = top;
+        top = top->next;
+        delete temp;
+    }
 }
 
 //****************************************************
-//										push(T&)
+//                                        isEmpty()
+template <class T>
+bool LinkedStack<T>::isEmpty(){
+    return ( top == NULL );
+}
+
+//****************************************************
+//                                        push(T&)
 template <class T>
 void LinkedStack<T>::push(T& item){
-	Node<T>* newNode = new node<T>();
-	newNode->data = item;
-	newNode->next = top;
-	top = newNode;
+    node<T>* newNode = new node<T>();
+    newNode->data = item;
+    newNode->next = top;
+    top = newNode;
+    length++;
 }
 
 //****************************************************
-//										pop()
+//                                        pop()
 template <class T>
 T LinkedStack<T>::pop(){
-	if ( !( this.isEmpty() ) ){
-		T topData = top->data;
-		top = top->next;
-		return topData;
-	}
-	else {
-		cout << "Stack is empty, nothing to pop..." << endl;
-	}
+    assert( !(this->isEmpty()) );
+    T topData = top->data;
+    top = top->next;
+    length--;
+    return topData;
+    
+    // old approach,
+    // failed because control may reach end of non-void function
+    // solution is use use assert.
+//    if ( !( this->isEmpty() ) ){
+//        T topData = top->data;
+//        top = top->next;
+//        length--;
+//        return topData;
+//    }
+//    else {
+//        cout << "Stack is empty, nothing to pop..." << endl;
+//        // conntrol may reach end of non-void function...............
+//        return NULL;
+//    }
+    
+    
 }
 
 //****************************************************
-//										<< operator
+//                                        << operator
 template <class T>
 ostream& operator<<(ostream& os, LinkedStack<T>& lStack){
-	for( int i=0; i < lStack.top; i++ ){
-		os << lStack.array[i] << " ";
-	}
-	return os;
+    node<T>* temp1;
+    temp1 = lStack.top;
+    for( int i=0; i < lStack.length; i++ ){
+        os << temp1->data << " ";
+        temp1 = temp1->next;
+    }
+    return os;
 }
 
 
 //****************************************************
-//										seeTop()
+//                                        seeTop()
 template <class T>
 T LinkedStack<T>::seeTop(){
-	if ( !( this.isEmpty() ) ){
-		return top->data;
-	} 
+    if ( !( this->isEmpty() ) ){
+        return top->data;
+    }
 }
 
 //****************************************************
-//										ssTop()
+//                                        ssTop()
 // not sure if I need this function...
 
 
+//****************************************************
+//                                        getLength()
+template <class T>
+int LinkedStack<T>::getLength(){
+    return length;
+}
 
 #endif /*LINKEDSTACK_H_*/
+
 
 
 
